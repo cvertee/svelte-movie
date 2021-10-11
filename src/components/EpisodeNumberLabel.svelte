f81284dbb531f06ead76f4562691416485252f6d
cver <cvertee@gmail.com>
2021-10-11 20:25:47 +0700

    Episode card info like name and S1E1

---
+++ src/components/EpisodeNumberLabel.svelte
@@ -0,0 +1,21 @@
+<script>
+    export let episodeData;
+</script>
+
+<main>
+    {#if episodeData.season !== null}
+        <div class='episode-label'>
+            S{episodeData.season}{episodeData.number ? `E${episodeData.number}` : ``}
+        </div>
+    {/if}
+</main>
+
+<style>
+    .episode-label {
+        position: absolute;
+        top: 0px;
+        background-color: rgba(94, 94, 94, 0.75);
+        color: white;
+        padding: 10px 10px;
+    }
+</style>
--- src/components/ShowEpisodeCard.svelte
+++ src/components/ShowEpisodeCard.svelte
@@ -1,8 +1,44 @@
 <script>
+    import EpisodeNumberLabel from './EpisodeNumberLabel.svelte';
+
     export let episodeData = {};
 </script>
 
 <main>
-    <img src={episodeData.image?.medium} alt={episodeData.name}>
-    <div>{episodeData.name}</div>
+    <div class='episode-card'>
+        <div class='episode-card-container'>
+            <EpisodeNumberLabel episodeData={episodeData}/>
+            <img src={episodeData.image ? episodeData.image.medium : '/images/placeholder.jpg'} alt={episodeData.name}>
+            <div class='episode-name'>{episodeData.name}</div>
+        </div>
+    </div>
 </main>
+
+<style>
+    .episode-card {
+        width: 25em;
+        margin: 1.5em 0em;
+    }
+
+    .episode-card img {
+        width: 100%;
+    }
+
+    .episode-card-container {
+        position: relative;
+    }
+
+    .episode-name {
+        position: absolute;
+        bottom: 0px;
+        background-color: gray;
+        color: white;
+        width: 100%;
+    }
+
+    .episode-label {
+        position: absolute;
+        top: 0px;
+    }
+
+</style>
